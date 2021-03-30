2578feb120a56955065ec9a97448f38e3d293913
Eason Lu <eason.lu@avepoint.com>
2021-03-30 14:40:54 -0400

    models

---
+++ Models/DeskInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeskReservation.Models
+{
+    public class DeskInfo
+    {
+        public int DeskId { get; set; }
+
+        public string DeskName { get; set; }
+
+        public string Description { get; set; }
+
+        public string OfficeName { get; set; }
+
+        public int MaxDesk { get; set; }
+
+    }
+}

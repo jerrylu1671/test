0af110f30286823db1ebc370901f1fd130347f0e
Jerry Lu <jerrylu1671@gmail.com>
2021-03-29 20:27:02 -0400

    new project

---
+++ Models/DeskReservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeskReservation.Models
+{
+    public class DeskReservation
+    {
+        public DateTime ReserveDate { get; set; }
+
+        public int DeskId { get; set; }
+
+        public string UserId { get; set; }
+
+    }
+}
